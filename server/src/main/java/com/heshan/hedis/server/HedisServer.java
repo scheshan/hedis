92126490b1a34f26228a41bc35ee7cd9c9bb25b8
heshan <5373827@qq.com>
2019-06-28 16:17:29 +0800

    basic server logic

---
+++ server/src/main/java/com/heshan/hedis/server/HedisServer.java
@@ -0,0 +1,68 @@
+package com.heshan.hedis.server;
+
+import com.heshan.hedis.server.handler.HedisCommandHandler;
+import com.heshan.hedis.shared.handler.HedisMessageDecoder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * HedisServer
+ *
+ * @author heshan
+ * @date 2019-06-28
+ */
+public class HedisServer {
+
+    private EventLoopGroup masterGroup;
+
+    private EventLoopGroup workerGroup;
+
+    private Channel channel;
+
+    public HedisServer() {
+        masterGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
+    }
+
+    public void start() throws Exception {
+        ServerBootstrap bootstrap = new ServerBootstrap()
+                .group(masterGroup, workerGroup)
+                .childHandler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new HedisMessageDecoder())
+                                .addLast(new HedisCommandHandler());
+                    }
+                })
+                .channel(NioServerSocketChannel.class);
+
+        channel = bootstrap.bind(16379).sync().channel();
+    }
+
+    public void stop() {
+        masterGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+        channel.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        HedisServer server = new HedisServer();
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(server.new ShutdownThread());
+    }
+
+    private class ShutdownThread extends Thread {
+
+        @Override
+        public void run() {
+            HedisServer.this.stop();
+        }
+    }
+}
---
+++ server/src/main/java/com/heshan/hedis/server/handler/HedisCommandHandler.java
@@ -0,0 +1,27 @@
+package com.heshan.hedis.server.handler;
+
+import com.heshan.hedis.shared.codec.AbstractHedisMessage;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * HedisCommandHandler
+ *
+ * @author heshan
+ * @date 2019-06-28
+ */
+public class HedisCommandHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        AbstractHedisMessage message = (AbstractHedisMessage) msg;
+        System.out.println(message.content());
+
+        ByteBuf buf = ctx.alloc().buffer();
+        buf.writeCharSequence("*-1\r\n", CharsetUtil.UTF_8);
+        ctx.writeAndFlush(buf);
+        buf.release();
+    }
+}
