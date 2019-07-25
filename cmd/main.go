b82f48f9ad03f0ae76f744408a3d2b407824e9aa
heshan <5373827@qq.com>
2019-07-25 14:13:20 +0800

    Add startup command

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/scheshan/hedis"
+)
+
+func main() {
+
+	c := new(hedis.ServerConfig)
+	c.Addr = ":16379"
+	ser := hedis.NewServer(c)
+	if err := ser.Start(); err != nil {
+		fmt.Println(err)
+	}
+
+	<-time.After(5 * time.Second)
+	ser.Stop()
+
+	fmt.Println("closed")
+
+	<-time.After(100000 * time.Second)
+}
