69b2a94290422e89198ffd9546f471a993faa659
heshan <5373827@qq.com>
2021-07-26 14:47:10 +0800

    server支持事件回调

--- server/server.go
+++ server/server.go
@@ -21,6 +21,24 @@ type Server interface {
 	Stop() error
 }
 
+type ServerEventHandler interface {
+	PostInit()
+	PreStart()
+	PostStart()
+	PreStop()
+	SessionCreated(s *Session)
+	SessionError(s *Session, err error)
+	SessionClosed(s *Session)
+}
+
+//baseServer 基础服务端，为了复用逻辑，提取出的所有服务端代码的基础结构。hedis服务端（标准服务、主从、哨兵、集群等），都应该尽量组合此结构。
+//
+//此结构实现了 Server 接口的方法，其他服务端结构不用再实现这些方法。如果有定制化需求，可以注册事件回调。
+//
+//此结构定义了与客户端的交互，包括连接管理、客户端读写等。其他服务端结构不用再处理这些逻辑，如果有定制化需求，可以注册事件回调。
+//
+//此结构包含的事件回调可以参考 ServerEventHandler。由于 baseServer 自己也实现了回调方法，其他服务端结构可以选择性的调用 baseServer
+//的回调方法，实现高度定制化。
 type baseServer struct {
 	commands      *core.Hash
 	config        *ServerConfig
@@ -35,11 +53,13 @@ type baseServer struct {
 	decoder       *codec.Decoder
 	encoder       *codec.Encoder
 	mutex         *sync.Mutex
+	srv           ServerEventHandler
 }
 
-func newBaseServer(config *ServerConfig) *baseServer {
+func newBaseServer(config *ServerConfig, srv ServerEventHandler) *baseServer {
 	s := &baseServer{}
 
+	s.srv = srv
 	s.config = config
 	s.commands = core.NewHash()
 	s.requests = make(chan *ClientMessage, 10240)
@@ -49,8 +69,6 @@ func newBaseServer(config *ServerConfig) *baseServer {
 	s.decoder = &codec.Decoder{}
 	s.encoder = &codec.Encoder{}
 	s.mutex = &sync.Mutex{}
-
-	s.initCommands()
 
 	return s
 }
@@ -83,12 +101,7 @@ func (t *baseServer) accept() {
 		}
 
 		session := t.initSession(tcpConn)
-		t.addSession(session)
-
-		go t.readLoop(session)
-		go t.writeLoop(session)
-
-		log.Printf("客户端 %s 连接成功", session)
+		t.srv.SessionCreated(session)
 	}
 }
 
@@ -138,16 +151,6 @@ func (t *baseServer) closeSession(session *Session) {
 	session.conn.Close()
 }
 
-func (t *baseServer) onSessionError(session *Session, err error) {
-	t.closeSession(session)
-
-	if err == io.EOF {
-		log.Printf("客户端 %s 主动关闭连接", session)
-		return
-	}
-	log.Printf("客户端 %s 连接出错，关闭连接: %v", session, err)
-}
-
 func (t *baseServer) writeToSession(session *Session, msg codec.Message) error {
 	err := t.encoder.Encode(session.writer, msg)
 	if err != nil {
@@ -161,7 +164,7 @@ func (t *baseServer) readLoop(session *Session) {
 	for t.running && session.state == SessionStateConnected {
 		msg, err := t.decoder.Decode(session.reader)
 		if err != nil {
-			t.onSessionError(session, err)
+			t.srv.SessionError(session, err)
 			return
 		}
 
@@ -185,7 +188,7 @@ func (t *baseServer) writeLoop(session *Session) {
 
 		err := t.writeToSession(session, msg)
 		if err != nil {
-			t.onSessionError(session, err)
+			t.srv.SessionError(session, err)
 			return
 		}
 	}
@@ -221,6 +224,45 @@ func (t *baseServer) processCommand() {
 	}
 }
 
+func (t *baseServer) PostInit() {
+	t.initCommands()
+}
+
+func (t *baseServer) PreStart() {
+
+}
+
+func (t *baseServer) PostStart() {
+
+}
+
+func (t *baseServer) PreStop() {
+
+}
+
+func (t *baseServer) SessionCreated(s *Session) {
+	t.addSession(s)
+
+	go t.readLoop(s)
+	go t.writeLoop(s)
+
+	log.Printf("客户端 %s 连接成功", s)
+}
+
+func (t *baseServer) SessionClosed(s *Session) {
+
+}
+
+func (t *baseServer) SessionError(s *Session, err error) {
+	t.closeSession(s)
+
+	if err == io.EOF {
+		log.Printf("客户端 %s 主动关闭连接", s)
+		return
+	}
+	log.Printf("客户端 %s 连接出错，关闭连接: %v", s, err)
+}
+
 func (t *baseServer) Start() error {
 	t.mutex.Lock()
 	defer func() {
@@ -231,8 +273,11 @@ func (t *baseServer) Start() error {
 		log.Println("Server is already running")
 		return errors.New("server is already running")
 	}
-
 	t.running = true
+
+	t.srv.PostInit()
+	t.srv.PreStart()
+
 	err := t.bindAndListen()
 	if err != nil {
 		return err
@@ -241,11 +286,13 @@ func (t *baseServer) Start() error {
 	go t.processCommand()
 	go t.accept()
 
+	t.srv.PostStart()
+
 	return nil
 }
 
 func (t *baseServer) Stop() {
-
+	t.PreStop()
 }
 
 //#region commands
--- server/server_standard.go
+++ server/server_standard.go
@@ -1,12 +1,97 @@
 package server
 
+import (
+	"errors"
+	"hedis/codec"
+	"hedis/core"
+)
+
 type StandardServer struct {
 	*baseServer
+
+	db []*Db
+}
+
+func (t *StandardServer) PostInit() {
+	t.baseServer.PostInit()
+
+	t.initDb()
+	t.initCommands()
+}
+
+func (t *StandardServer) PreStart() {
+	t.baseServer.PreStart()
+}
+
+func (t *StandardServer) PostStart() {
+	t.baseServer.PostStart()
+}
+
+func (t *StandardServer) PreStop() {
+	t.baseServer.PreStop()
+}
+
+func (t *StandardServer) SessionCreated(s *Session) {
+	t.baseServer.SessionCreated(s)
+
+	t.changeSessionDb(s, 0)
+}
+
+func (t *StandardServer) SessionError(s *Session, err error) {
+	t.baseServer.SessionError(s, err)
+}
+
+func (t *StandardServer) SessionClosed(s *Session) {
+	t.baseServer.SessionClosed(s)
 }
 
 func NewStandardServer(config *ServerConfig) *StandardServer {
 	srv := &StandardServer{}
-	srv.baseServer = newBaseServer(config)
+	srv.baseServer = newBaseServer(config, srv)
 
 	return srv
 }
+
+func (t *StandardServer) initDb() {
+	dbNum := 16
+
+	t.db = make([]*Db, dbNum)
+	for i := 0; i < dbNum; i++ {
+		t.db[i] = NewDb()
+	}
+}
+
+func (t *StandardServer) initCommands() {
+	t.addCommand("select", t.CommandSelect)
+}
+
+func (t *StandardServer) changeSessionDb(s *Session, db int) error {
+	if db < 0 || db >= len(t.db) {
+		return errors.New("invalid db")
+	}
+
+	s.db = t.db[db]
+	return nil
+}
+
+//#region connection commands
+
+func (t *StandardServer) CommandSelect(s *Session, args []*core.String) codec.Message {
+	if len(args) != 1 {
+		return MessageErrorInvalidArgNum
+	}
+
+	dbNum, err := args[0].ToInt()
+	if err != nil {
+		return codec.NewErrorErr(err)
+	}
+
+	err = t.changeSessionDb(s, dbNum)
+	if err != nil {
+		return codec.NewErrorErr(err)
+	}
+
+	return MessageSimpleOK
+}
+
+//#endregion
